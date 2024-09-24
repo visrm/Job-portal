93eeeb892bf3d17a2bd6d4d29ec1f5464a21a5c9
Rahul <yveltal4286@gmail.com>
2024-09-24 19:53:22 +0530

    :heavy_plus_sign: update prior frontend files

--- Frontend/src/components/HomePage.jsx
+++ Frontend/src/components/HomePage.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
+import AppNavBar from "./Navigation/AppNavBar";
 
 const HomePage = () => {
   return (
     <>
-       <h1 align="center"> Opening page</h1>
+      <AppNavBar />
+      <h1 align="center"> Opening page</h1>
     </>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
--- Frontend/src/components/Navigation/AppNavBar.jsx
+++ Frontend/src/components/Navigation/AppNavBar.jsx
@@ -1,45 +1,53 @@
 import React from "react";
-import { AppBar, Toolbar, Typography, Stack, Button, styled } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Stack,
+  Button,
+  styled,
+} from "@mui/material";
 import { Link } from "react-router-dom";
 
 const NavButton = styled(Button)({
   padding: "0.5rem 1rem",
-   fontFamily: "Poppins, sans-serif",
+  fontFamily: "Poppins, sans-serif",
   color: "white",
   fontWeight: 600,
   fontKerning: "normal",
   borderRadius: "1.5rem",
-   boxShadow: 'none',
-  '&:hover': {
-    backgroundColor: '#0069d9',
+  boxShadow: "none",
+  "&:hover": {
+    backgroundColor: "#0069d9",
   },
-  '&:active': {
-    backgroundColor: '#0062cc',
+  "&:active": {
+    backgroundColor: "#0062cc",
   },
 });
 
 const AppNavBar = () => {
   return (
     <>
-      <AppBar position="sticky">
+      <AppBar position="sticky"  sx={{ padding: "0 0.5rem" }}>
         <Toolbar>
           <Typography
             variant="h5"
             component="div"
-            sx={{ mr: "auto", fontFamily: "Poppins, sans-serif", fontKerning: "normal", fontWeight: "800" }}
+            sx={{
+              mr: "auto",
+              fontFamily: "Poppins, sans-serif",
+              fontKerning: "normal",
+              fontWeight: "800",
+            }}
           >
-          Job Portal
+            Job Portal
           </Typography>
           <Stack direction="row" spacing={0.5}>
             <Link to="/">
-              <NavButton variant="text" >
-                Jobs
-              </NavButton>
+              <NavButton variant="text">Home</NavButton>
             </Link>
             <Link to="/login">
-              <NavButton variant="text" >
-                Login / Signup
-              </NavButton>
+              <NavButton variant="text">Login / Signup</NavButton>
             </Link>
           </Stack>
         </Toolbar>
