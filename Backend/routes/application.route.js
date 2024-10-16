450ed34a7b30c58574c034d7947ebfb343184019
Rahul <77574839+visrm@users.noreply.github.com>
2024-10-16 13:41:07 +0530

    Add files via upload

---
+++ Backend/routes/application.route.js
@@ -0,0 +1,12 @@
+import express from "express";
+import isAuthenticated from "../middleware/isAuthenticated.js";
+import { applyJob, getApplicants, getAppliedJobs, updateStatus } from "../controllers/application.controller.js";
+
+const router = express.Router();
+
+router.route("/apply/:jobId").get(isAuthenticated, applyJob);
+router.route("/get").get(isAuthenticated, getAppliedJobs);
+router.route("/:id/applicants").get(isAuthenticated, getApplicants);
+router.route("/status/:id/update").post(isAuthenticated, updateStatus);
+
+export default router;
