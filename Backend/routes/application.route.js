8734040877652ae04112b7abefa65dd20f2f512a
Rahul <77574839+visrm@users.noreply.github.com>
2024-10-16 13:48:49 +0530

    Update application.route.js

--- Backend/routes/application.route.js
+++ Backend/routes/application.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import isAuthenticated from "../middleware/isAuthenticated.js";
+import isAuthenticated from "../middlewares/authentication.js";
 import { applyJob, getApplicants, getAppliedJobs, updateStatus } from "../controllers/application.controller.js";
 
 const router = express.Router();
