6ec5b5dc3f7e71a0b533b999e2ed4a13b0a96325
Rahul <yveltal4286@gmail.com>
2024-10-20 17:49:14 +0530

    :heavy_plus_sign: manage-jobs initiated

--- Frontend/src/components/adminModule/ManageJobs.jsx
+++ Frontend/src/components/adminModule/ManageJobs.jsx
@@ -1,70 +1,184 @@
-import React from "react";
+import { useState } from "react";
 import AdminNavigation from "./AdminNavigation";
 import {
   Button,
   FormControl,
-  InputLabel,
-  MenuItem,
-  Select,
-  Stack,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
   TextField
 } from "@mui/material";
+import axios from "axios";
+import { JOB_API_END_POINT } from "../../utils/constants";
 
 const ManageJobs = () => {
+  const [input, setInput] = useState({
+    title: "",
+    description: "",
+    requirements: "",
+    salary: "",
+    location: "",
+    type: "",
+    vacancy: "",
+    companyId: ""
+  });
+  async function handlePostSubmit(e) {
+    try {
+      e.preventDefaults();
+      const jobData = {
+        title: input.title,
+        description: input.description,
+        requirements: input.requirements,
+        salary: input.salary,
+        location: input.location,
+        jobType: input.type,
+        vacancy: input.vacancy,
+        companyId: input.companyId
+      };
+      const res = await axios.post(`${JOB_API_END_POINT}/post`, jobData, {
+        headers: {
+          "Content-Type": "application/json"
+        },
+        withCredentials: true
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const handleChange = (e) => {
+    setInput(...input, { [e.target.name]: e.target.value });
+  };
+
   return (
     <>
       <header>
         <AdminNavigation />
       </header>
-      <h3>ADD JOBS</h3>
-      <form method="POST">
-        <Stack>
-          
-        </Stack>
-        <TextField
-          required
-          id="outlined-required"
-          label="Job position"
-          variant="filled"
-        ></TextField>
-        <TextField
-          required
-          id="outlined-required"
-          label="Description"
-          variant="filled"
-        ></TextField>
-        <TextField
-          required
-          id="outlined-required"
-          label="Requirements"
-          variant="filled"
-        ></TextField>
-        <TextField
-          required
-          id="outlined-required"
-          label="Location"
-          variant="filled"
-        ></TextField>
-        <TextField
-          required
-          id="outlined-required"
-          label="Salary"
-          variant="filled"
-        ></TextField>
-        <FormControl variant="filled" required>
-          <InputLabel>Job type </InputLabel>
-          <Select
-            sx={{ width: "24ch" }}
-            id="job-type"
-            defaultChecked="Part-time"
-          >
-            <MenuItem Value="Part-time">Part-time</MenuItem>
-            <MenuItem Value="Full-time">Full-time</MenuItem>
-            <MenuItem value="Internship">Internship</MenuItem>
-          </Select>
-        </FormControl>
-        <Button variant="contained">Add</Button>
-      </form>
+      <main>
+        <h3 align="center">Add Job</h3>
+        <form
+          method="POST"
+          onSubmit={handlePostSubmit}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            flexWrap: "nowrap",
+            gap: "0.25rem",
+            width: "100%",
+            maxWidth: "33%",
+            margin: "0.25rem auto"
+          }}
+        >
+          <div style={{}}></div>
+          <FormControl className="form-field">
+            <label htmlFor="title">Job Title</label>
+            <TextField
+              required
+              onChange={handleChange}
+              value={input.title}
+              name="title"
+              id="title"
+              placeholder="Senior Web Developer"
+              variant="outlined"
+              size="small"
+              fullWidth
+            />
+          </FormControl>
+          <FormControl className="form-field">
+            <label htmlFor="description">Description</label>
+            <TextField
+              required
+              onChange={handleChange}
+              value={input.description}
+              name="description"
+              id="description"
+              placeholder="Proficient in Web Development tech"
+              variant="outlined"
+              size="small"
+              fullWidth
+            />
+          </FormControl>
+          <FormControl className="form-field">
+            <label htmlFor="requirements">Requirements</label>
+            <TextField
+              required
+              onChange={handleChange}
+              value={input.requirements}
+              name="requirements"
+              id="requirements"
+              placeholder="HTML/CSS, tailwindcss, Bootstrap etc."
+              variant="outlined"
+              size="small"
+              fullWidth
+            />
+          </FormControl>
+          <FormControl className="form-field">
+            <label htmlFor="location">Location</label>
+            <TextField
+              required
+              onChange={handleChange}
+              value={input.location}
+              name="location"
+              id="location"
+              placeholder="Vadakara, Kerala, India"
+              variant="outlined"
+              size="small"
+              fullWidth
+            />
+          </FormControl>
+          <FormControl className="form-field">
+            <label htmlFor="salary">Salary</label>
+            <TextField
+              required
+              onChange={handleChange}
+              value={input.salary}
+              name="salary"
+              id="salary"
+              placeholder="120000"
+              variant="outlined"
+              size="small"
+              fullWidth
+            />
+          </FormControl>
+
+          <FormControl className="form-field">
+            <label htmlFor="job-type" className="form-label">
+              Job Type:
+            </label>
+            <RadioGroup
+              row
+              required
+              defaultValue="Full-time"
+              onChange={handleChange}
+              name="jobType"
+              value={input.type}
+              id="job-type"
+            >
+              <FormControlLabel
+                value="Full-time"
+                control={<Radio checked={input.type === "Full-time"} />}
+                label="Full-time"
+              />
+              <FormControlLabel
+                value="Part-time"
+                control={<Radio checked={input.type === "Part-time"} />}
+                label="Part-time"
+              />
+              <FormControlLabel
+                value="Internship"
+                control={<Radio checked={input.type === "Internship"} />}
+                label="Internship"
+              />
+            </RadioGroup>
+          </FormControl>
+          <FormControl className="form-field">
+            <Button variant="contained" type="submit">
+              Add
+            </Button>
+          </FormControl>
+        </form>
+      </main>
     </>
   );
 };
