57f24da7b9ef38246f25ba04ca7a6001ef9c9fb6
Rahul <yveltal4286@gmail.com>
2024-10-20 13:40:45 +0530

    :heavy_plus_sign: update userModel.js

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -2,36 +2,23 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    fullname: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    phoneNo: {
-      type: Number,
-      required: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
+    fullname: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    phoneNo: { type: Number, required: true },
+    password: { type: String, required: true },
     profile: {
       bio: { type: String, default: "" },
       skills: { type: String },
       resume: { type: String }, // URL to resume file
-      company: { type: String },
-      profileImg: { type: String, default: "" },
+      company: { type: mongoose.Schema.Types.ObjectId, ref: "Company" },
+      profileImg: { type: String, default: "" }
     },
     role: {
       type: String,
       enum: ["admin", "user"],
       default: "user",
-      required: true,
-    },
+      required: true
+    }
   },
   { timestamps: true }
 );
